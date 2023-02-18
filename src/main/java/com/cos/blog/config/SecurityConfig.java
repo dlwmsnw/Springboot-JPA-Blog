7557a083e61aaf6607edb7133ec766404074bd85
dlwmsnw <dlwmsnw@gmail.com>
2023-02-19 04:56:03 +0900

    2/19

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.cos.blog.config;
 
 import org.aspectj.weaver.ast.And;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.config.auth.PrincipalDetailService;
 
 
 @Configuration // 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것. (IoC에서 관리가 된다.)
@@ -14,16 +21,35 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 // 위에 세 가지는 세트다. (이해가 안 가면 그냥 시큐리티 할 때 이 세 가지 걸면 됨.)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private PrincipalDetailService principalDetailService;
+	
+	@Bean // IoC가 된다.
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// 시큐리티가 대신 로그인해주는데 password를 가로채기를 하는데,
+	// 해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지 알아야
+	// 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable() // csrf 토큰 비활성화 (테스트 시 걸어두는 게 좋음)
 			.authorizeRequests()
-			    .antMatchers("/auth/**")
+			    .antMatchers("/", "/auth/**", "/js/**", "/css**", "/image/**")
 		  	   .permitAll()
 			   .anyRequest()
 			   .authenticated()
 			.and()
 				.formLogin()
-				.loginPage("/auth/loginForm");
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc") // 스프링 시큐리티가 해당 주소로 요청 오는 로그인을 가로채서 대신 로그인 해 준다.
+				.defaultSuccessUrl("/") ; // 로그인이 정상적으로 요청이 되면 "/" 주소로 이동.
 	}
 }// class() end
--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,6 @@ public class UserApiController {
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
 		System.out.println("UserApiController : save 호출됨");
 		// 실제로 DB에 insert를 하고 아래에서 return이 되면 된다.
-		user.setRole(RoleType.USER);
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
@@ -39,5 +39,4 @@ public class UserApiController {
 	 * 하면 session이 만들어진다. } return new ResponseDto<Integer>(HttpStatus.OK.value(),
 	 * 1); }
 	 */
-
 }// class() end

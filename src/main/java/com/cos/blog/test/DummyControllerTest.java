0df232e8ba064580ce9e2df2e298ede6cf315e29
dlwmsnw <dlwmsnw@gmail.com>
2023-02-21 06:02:15 +0900

    2/21

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -39,11 +39,11 @@ public class DummyControllerTest {
 	//한 페이지당 2건에 데이터를 리턴받아 볼 예정.
 	//http://localhost:8000/blog/dummy/user
 	@GetMapping("/dummy/user")
-	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+	public Page<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
 		Page<User> pagingUsers = userRepository.findAll(pageable);
 		
 		List<User> users = pagingUsers.getContent();
-		return users;
+		return pagingUsers;
 	}
 	
 	
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,7 +3,11 @@ let index = {
 		$("#btn-save").on("click", () => {
 			this.save();
 		});
+		$("#btn-delete").on("click", () => {
+			this.deleteById();
+		});
 	},
+
 
 	save: function() {
 		//alert('user의 save함수 호출됨');
@@ -19,7 +23,22 @@ let index = {
 			contentType: "application/json; charset=utf-8", // body데이터가 어떤 타입인지(MIME). data&contentType는 세트다.
 			dataType: "json" // 요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴 게 json이라면) => javascript오브젝트로 변경
 		}).done(function(resp) {
-			alert("글 쓰기가 완료되었습니다.");
+			alert("글쓰기가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+
+	deleteById: function() {
+		let id = $("#id").text();
+
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/" + id,
+			dataType: "json"
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));

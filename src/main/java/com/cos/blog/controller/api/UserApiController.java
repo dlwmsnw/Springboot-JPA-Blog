32725f10e121bd9dd8b2d1f33dd5b7906d50fd7a
dlwmsnw <dlwmsnw@gmail.com>
2023-02-21 22:12:34 +0900

    2/21

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -4,8 +4,13 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,9 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
 		System.out.println("UserApiController : save 호출됨");
@@ -28,15 +36,17 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 	
-	/* 전통적인 로그인 방식.
-	 * 위에 있는 시큐리티를 이용해서 로그인을 할 것이기 때문에 주석.
-	 * @PostMapping("/api/user/login") public ResponseDto<Integer>
-	 * login(@RequestBody User user, HttpSession session){
-	 * System.out.println("UserApiController : save 호출됨"); User principal =
-	 * userService.로그인(user); // principal (접근 주체)
-	 * 
-	 * if(principal != null) { session.setAttribute("principal", principal); // 이렇게
-	 * 하면 session이 만들어진다. } return new ResponseDto<Integer>(HttpStatus.OK.value(),
-	 * 1); }
-	 */
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user) { // key=value, x-www-form-urlencoded
+		userService.회원수정(user);
+		// 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐음.
+		// 하지만 세션값은 변경되지 않은 상태이기 때문에 우리가 직접 세션값을 변경해줄 것임.
+		// 세션 등록
+
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+	
 }// class() end
